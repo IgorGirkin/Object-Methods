774543e50c201c40610202d03590387ed694f950
grisha <norsulfasol@gmail.com>
2022-11-03 01:06:10 +0300

    ДЗ Методы объектов.

---
+++ src/Book.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+
+public class Book {
+    final private String nameBook;
+    final private Author fullNameAuthor;
+    private int yearOfPublication;
+
+    public Book(String nameBook, Author fullNameAuthor, int yearOfPublication) {
+        this.nameBook = nameBook;
+        this.fullNameAuthor = fullNameAuthor;
+        this.yearOfPublication = yearOfPublication;
+    }
+    public String toString (Author fullNameAuthor) {
+        return fullNameAuthor.toString();
+    }
+
+    public Author getFullNameAuthor() {
+        return fullNameAuthor;
+    }
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    public Author getNameAuthor() {
+        return fullNameAuthor;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublication == book.yearOfPublication && nameBook.equals(book.nameBook) && fullNameAuthor.equals(book.fullNameAuthor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameBook, fullNameAuthor, yearOfPublication);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author strugatsky = new Author("Борис и Аркадий", "Стругатские");
+        Book mondayBeginsOnSaturday = new Book("Понедельник начинается в суббо" +
+                "ту", strugatsky, 1964);
+        Author strugatsky2 = new Author("Борис и Аркадий", "Стругатские");
+        Book mondayBeginsOnSaturday2 = new Book("Понедельник начинается в суббо" +
+                "ту", strugatsky2, 1964);
+
+        Author marcusAurelius = new Author("Марк Аврелий", "Марк Элий Аврелий Вер Цезарь");
+        Book reasoningAboutYourself = new Book("Рассуждения о самом себ" +
+                "е.", marcusAurelius, 1914);
+        Book reasoningAboutYourself2 = new Book("Рассуждения о самом себ" +
+                "е.", marcusAurelius, 1914);
+        mondayBeginsOnSaturday.setYearOfPublication(1979);
+
+        System.out.println(reasoningAboutYourself.getFullNameAuthor());
+        System.out.println(reasoningAboutYourself.getNameBook());
+        System.out.println(reasoningAboutYourself.getYearOfPublication());
+        System.out.println("------------------------------------------");
+
+        System.out.println(mondayBeginsOnSaturday.getFullNameAuthor());
+        System.out.println(mondayBeginsOnSaturday.getNameBook());
+        System.out.println(mondayBeginsOnSaturday.getYearOfPublication());
+
+        System.out.println(strugatsky.equals(strugatsky2));
+        System.out.println(mondayBeginsOnSaturday.equals(mondayBeginsOnSaturday2));//Другой год издания
+        System.out.println(reasoningAboutYourself.equals(reasoningAboutYourself2));
+        if (reasoningAboutYourself.hashCode() != reasoningAboutYourself2.hashCode()) {
+            System.out.println(false);
+        } else {
+            System.out.println(true);
+        }
+
     }
 }
